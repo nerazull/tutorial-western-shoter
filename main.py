936c4117cf77a2f272a3790b56dc9e94da73527c
nerazull <66993587+nerazull@users.noreply.github.com>
2024-02-25 15:26:11 +0200

    Created Player class (Lesson 110): - Defined input and movement methods

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from settings import *
+from player import Player
 
 class Game:
 	def __init__(self):
@@ -7,6 +8,14 @@ class Game:
 		self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 		pygame.display.set_caption('Western Shooter')
 		self.clock = pygame.time.Clock()
+
+		# groups
+		self.all_sprites = pygame.sprite.Group()
+
+		self.setup()
+
+	def setup(self):
+		Player((200,200), self.all_sprites, None, None)
 
 	def run(self):
 		while True:
@@ -16,6 +25,13 @@ class Game:
 					pygame.quit()
 					sys.exit()
 			dt = self.clock.tick() / 1000
+
+			# update groups
+			self.all_sprites.update(dt)
+
+			# draw groups
+			self.display_surface.fill('black')
+			self.all_sprites.draw(self.display_surface)
 
 			pygame.display.update()
 
